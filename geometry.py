04a37d062fb5d9cd02b363fbb665a7e780004a74
JIntergalactical <jerzykwilczek@interia.pl>
2020-12-30 02:01:28 +0100

    add Rectangle class

--- geometry.py
+++ geometry.py
@@ -1,3 +1,21 @@
-from typing import Tuple
+from typing import Tuple, List, Optional
 
 Point = Tuple[float, float]
+
+
+class Rectangle:
+    def __init__(self, min_x: float, max_x: float, min_y: float, max_y: float):
+        self.min_x: float = min_x
+        self.max_x: float = max_x
+        self.min_y: float = min_y
+        self.max_y: float = max_y
+
+
+def rectangle_from_points(points: List[Point]) -> Optional[Rectangle]:
+    x_coords = list(map(lambda p: p[0], points))
+    y_coords = list(map(lambda p: p[1], points))
+    min_x = min(x_coords)
+    max_x = max(x_coords)
+    min_y = min(y_coords)
+    max_y = max(y_coords)
+    return Rectangle(min_x, max_x, min_y, max_y) if min_x != max_x and min_y != max_y else None
