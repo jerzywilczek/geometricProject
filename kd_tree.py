621ba0ad15ce6e0ba86892a6ba4f85b63b43dee9
jerzywilczek <jerzywilczek@student.agh.edu.pl>
2020-12-30 02:56:46 +0100

    modify _Node further

--- kd_tree.py
+++ kd_tree.py
@@ -1,12 +1,5 @@
-from enum import Enum, unique
 from typing import List, Callable
-from geometry import Point, Rectangle, rectangle_from_points
-
-
-@unique
-class _AxisType(Enum):
-    X = 0
-    Y = 1
+from geometry import Point, Rectangle, rectangle_from_points, AxisType
 
 
 class _Node:
@@ -16,16 +9,22 @@ class _Node:
 
         self.area = area if area is not None else rectangle_from_points(points)
 
-        self.division_axis_type: _AxisType = _AxisType[depth % 2]
+        self.division_axis_type: AxisType = AxisType[depth % 2]
         self.__point_comparing_key: Callable[[Point], float] = \
-            (lambda p: p[0]) if self.division_axis_type is _AxisType.Y else (lambda p: p[1])
+            (lambda p: p[0]) if self.division_axis_type is AxisType.Y else (lambda p: p[1])
 
         median = self.__median()
         self.dividing_line: float = median if not self.is_leaf else None
-        self.left = _Node(list(filter(lambda p: self.__point_comparing_key(p) <= median, points)), depth=depth + 1) \
-            if not self.is_leaf else None
-        self.right = _Node(list(filter(lambda p: self.__point_comparing_key(p) > median, points)), depth=depth + 1) \
-            if not self.is_leaf else None
+        self.left = _Node(
+            list(filter(lambda p: self.__point_comparing_key(p) <= median, points)),
+            area=area.less_than(median, self.division_axis_type),
+            depth=depth + 1
+        ) if not self.is_leaf else None
+        self.right = _Node(
+            list(filter(lambda p: self.__point_comparing_key(p) > median, points)),
+            area=area.greater_than(median, self.division_axis_type),
+            depth=depth + 1
+        ) if not self.is_leaf else None
 
     def __median(self) -> float:
         temp = sorted(list(map(self.__point_comparing_key, self.points)))
