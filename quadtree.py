2ee4cd8e11e40ae4dd31802f16c9115290ae45b1
stlenyk <stlenyk@gmail.com>
2021-01-01 21:17:14 +0100

    finish find and start view

--- quadtree.py
+++ quadtree.py
@@ -1,7 +1,10 @@
 from enum import IntEnum
 from typing import List
+import random
+import copy
+from draw_tool import *
 
-from geometry import Point, Rectangle, rectangle_from_points
+from geometry import Point, Rectangle
 
 
 class Quadrant(IntEnum):
@@ -11,7 +14,7 @@ class Quadrant(IntEnum):
     SE = 3
 
 
-class Node:
+class _Node:
 
     def __init__(self, n, s, w, e, quadrant, parent):
         self.pos = None
@@ -19,13 +22,13 @@ class Node:
         self.min_y = s
         self.min_x = w
         self.max_x = e
-        self.mid_y = (self.max_y + self.min_y) / 2
-        self.mid_x = (self.min_x + self.min_y) / 2
+        self.mid_y = (self.min_y + self.max_y) / 2
+        self.mid_x = (self.min_x + self.max_x) / 2
         self.quadrant = quadrant
         self.parent = parent
         self.children = None
 
-    def in_boundary(self, point: Point) -> bool:
+    def in_boundary(self, point: Point) -> bool:  # todo zmienić słabe nierówności + nie jest używane
         return self.min_y <= point[1] <= self.max_y and self.min_x <= point[0] <= self.max_x
 
     def add_child(self, node):
@@ -41,22 +44,24 @@ class Quadtree:
         s = min(points, key=lambda p: p[1])[1]
         w = min(points, key=lambda p: p[0])[0]
         e = max(points, key=lambda p: p[0])[0]
-        self.root = Node(n, s, w, e, None, None)
+        self.root = _Node(n, s, w, e, None, None)
+        points = copy.deepcopy(points)
+        random.shuffle(points)
         self.__create_quadtree(self.root, points)
 
-    def __create_quadtree(self, node: Node, points):
+    def __create_quadtree(self, node: _Node, points: List[Point]):
         if len(points) == 1:
             node.pos = points[0]
         if len(points) <= 1:
             return
-        ne = Node(node.max_y, node.mid_y, node.mid_x, node.max_x, Quadrant.NE, node)
-        nw = Node(node.max_y, node.mid_y, node.min_x, node.mid_x, Quadrant.NW, node)
-        sw = Node(node.mid_y, node.min_y, node.min_x, node.mid_x, Quadrant.SW, node)
-        se = Node(node.mid_y, node.min_y, node.mid_x, node.max_x, Quadrant.SE, node)
+        ne = _Node(node.max_y, node.mid_y, node.mid_x, node.max_x, Quadrant.NE, node)
+        nw = _Node(node.max_y, node.mid_y, node.min_x, node.mid_x, Quadrant.NW, node)
+        sw = _Node(node.mid_y, node.min_y, node.min_x, node.mid_x, Quadrant.SW, node)
+        se = _Node(node.mid_y, node.min_y, node.mid_x, node.max_x, Quadrant.SE, node)
 
         points_ne = [p for p in points if p[0] > node.mid_x and p[1] >= node.mid_y]
-        points_nw = [p for p in points if p[0] < node.mid_x and p[1] >= node.mid_y]
-        points_sw = [p for p in points if p[0] <= node.mid_x and p[1] < node.mid_y]
+        points_nw = [p for p in points if p[0] <= node.mid_x and p[1] > node.mid_y]
+        points_sw = [p for p in points if p[0] < node.mid_x and p[1] <= node.mid_y]
         points_se = [p for p in points if p[0] >= node.mid_x and p[1] < node.mid_y]
 
         node.add_child(ne)
@@ -64,11 +69,11 @@ class Quadtree:
         node.add_child(nw)
         self.__create_quadtree(nw, points_nw)
         node.add_child(sw)
-        self.__create_quadtree(ne, points_sw)
+        self.__create_quadtree(sw, points_sw)
         node.add_child(se)
         self.__create_quadtree(se, points_se)
 
-    def __find(self, node: Node, rect: Rectangle, res: List[Point]):
+    def __find(self, node: _Node, rect: Rectangle, res: List[Point]):
         if rect.min_x > node.max_x or rect.max_x < node.min_x or rect.min_y > node.max_y or rect.max_y < node.max_y:
             return
         if node.children is None:
@@ -84,8 +89,30 @@ class Quadtree:
         return res
 
 
-points = [tuple([0, 1]), tuple([5, 2]), tuple([2, 2]), tuple([5, 3]), tuple([2, 3]), tuple([6, 2])]
-Qt = Quadtree(points)
-rect = Rectangle(1, 4, 1, 3)
+class View:
 
-print(Qt.find(rect))
+    def __init__(self, points: List[Point], rect: Rectangle, quadtree: Quadtree):
+        self.scenes = []
+        self.quadrants = []
+        self.points = PointsCollection(points)
+        self.rect = LinesCollection(
+            [[(rect.min_x, rect.min_y), (rect.max_x, rect.min_y)], [(rect.max_x, rect.min_y), (rect.max_x, rect.max_y)],
+             [(rect.max_x, rect.max_y), (rect.min_x, rect.max_y)]],
+            color='brown')
+
+    def __gen_quadrants(self, node: _Node):
+        self.quadrants.extend(
+            [[(node.min_x, node.min_y), (node.max_x, node.min_y)], [(node.max_x, node.min_y), (node.max_x, node.max_y)],
+             [(node.max_x, node.max_y), (node.min_x, node.max_y)]])
+        if node.children is not None:
+            for ch in node.children:
+                if ch is not None:
+                    self.__gen_quadrants(node)
+
+
+points0 = [tuple([0.0, 1.0]), tuple([5.0, 2.0]), tuple([2.0, 2.0]), tuple([5.0, 3.0]), tuple([0.0, 0.0]),
+           tuple([2.0, 3.0]), tuple([6.0, 2.0]), tuple([3.0, 2.0])]
+Qt = Quadtree(points0)
+rect0 = Rectangle(1, 4, 1, 3)
+
+print(Qt.find(rect0))
