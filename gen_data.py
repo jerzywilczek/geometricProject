00142f83e3b81c5b20e66241ffe548ae3f402733
stlenyk <stlenyk@gmail.com>
2021-01-02 16:18:10 +0100

    add quadtree to visualizer.ipynb

--- gen_data.py
+++ gen_data.py
@@ -1,13 +1,13 @@
 from random import uniform
 from typing import Tuple
-from geometry import rectangle_from_points
+from geometry import rectangle_from_points, Rectangle
 
 
 def gen_points(scope: Tuple[float, float] = (0, 100), n: int = 100):
     return [(uniform(scope[0], scope[1]), uniform(scope[0], scope[1])) for _ in range(n)]
 
 
-def gen_rect(scope: Tuple[float, float] = (0, 100)):
+def gen_rect(scope: Tuple[float, float] = (0, 100)) -> Rectangle:
     result = None
     while result is None:
         result = rectangle_from_points([(uniform(scope[0], scope[1]), uniform(scope[0], scope[1])) for _ in range(2)])
