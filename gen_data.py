45724d9a5f6432f4a6365b94bcd7631b49c4f5a8
stlenyk <stlenyk@gmail.com>
2021-01-02 03:12:30 +0100

    rename gen_rect() parameters

--- gen_data.py
+++ gen_data.py
@@ -7,8 +7,5 @@ def gen_points(scope: Tuple[float, float] = (0, 100), n: int = 100):
     return [(uniform(scope[0], scope[1]), uniform(scope[0], scope[1])) for _ in range(n)]
 
 
-def gen_rect(r: Tuple[float, float] = (0, 100)):
-    return rectangle_from_points([(uniform(r[0], r[1]), uniform(r[0], r[1])) for _ in range(2)])
-
-
-
+def gen_rect(scope: Tuple[float, float] = (0, 100)):
+    return rectangle_from_points([(uniform(scope[0], scope[1]), uniform(scope[0], scope[1])) for _ in range(2)])
