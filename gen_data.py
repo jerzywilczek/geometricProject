477692d892f8c36e39800b455946ca58314b9833
jerzywilczek <jerzywilczek@student.agh.edu.pl>
2021-01-02 03:18:41 +0100

    add visualizer for kd-tree

--- gen_data.py
+++ gen_data.py
@@ -8,4 +8,7 @@ def gen_points(scope: Tuple[float, float] = (0, 100), n: int = 100):
 
 
 def gen_rect(scope: Tuple[float, float] = (0, 100)):
-    return rectangle_from_points([(uniform(scope[0], scope[1]), uniform(scope[0], scope[1])) for _ in range(2)])
+    result = None
+    while result is None:
+        result = rectangle_from_points([(uniform(scope[0], scope[1]), uniform(scope[0], scope[1])) for _ in range(2)])
+    return result
